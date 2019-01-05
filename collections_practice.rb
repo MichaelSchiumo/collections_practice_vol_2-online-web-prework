bbc66f5adc22e816d8bbe22acafe6cde62596bdf
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-05 03:37:04 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,12 @@
 # your code goes here
+
+def begins_with_r(array)
+ dictionary.select { |array| array.start_with?('a') }
+end 
+
+def contain_a(array)
+array.select { |str| str.include?('a') }
+end 
+
+
+
